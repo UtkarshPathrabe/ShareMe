d8202c7d6de6f228cee7e4d88ae622964ac61587
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-23 20:13:52 +0530

    Update App.js

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Login } from './components';
 import Home from './container/Home';
+import { getUserDataFromToken } from './utilities';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = getUserDataFromToken();
+    if (!userInfo) {
+      localStorage.clear();
+      navigate('/login');
+    }
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path='login' element={<Login />} />
