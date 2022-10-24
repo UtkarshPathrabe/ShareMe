60151191098d8fb651262786d007dd1e57ad805f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-24 17:57:45 +0530

    Update Pin.jsx

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -85,7 +85,7 @@ const Pin = ({ pin }) => {
                 </a>
               </div>
               {alreadySaved ? (
-                <button type="button" className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
+                <button type="button" className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none cursor-zoom-in">
                   {save?.length} Saved
                 </button>
               ) : (
