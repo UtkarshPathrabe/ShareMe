09c38f00f726f8f87b11dc759a8618514f47145d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-25 12:01:35 +0530

    Update Search.jsx

--- shareme_frontend/src/components/Search.jsx
+++ shareme_frontend/src/components/Search.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Search = () => {
+import { MasonryLayout, Spinner } from '.';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utilities/data';
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    } else {
+      setLoading(true);
+      client.fetch(feedQuery())
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    }
+  }, [searchTerm]);
+
   return (
-    <div>Search</div>
+    <div>
+      {loading ? <Spinner message="Searching for pins..." /> : null}
+      {(pins?.length !== 0) ? (<MasonryLayout pins={pins} />) : null}
+      {(pins?.length === 0 && searchTerm !== '' && !loading) ? (
+        <div className='mt-10 text-center text-xl'>
+          No Pins Found!
+        </div>
+      ) : null}
+    </div>
   );
 };
 
