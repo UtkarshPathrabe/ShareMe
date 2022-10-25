09c0e3f0051480ced9e8ccd5ed9526efa3d17d6d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-25 15:39:36 +0530

    Update client.js

--- shareme_frontend/src/client.js
+++ shareme_frontend/src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2021-11-16',
-  useCdn: true,
+  useCdn: false,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
